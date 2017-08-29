ec079f75638bae8e3b6297f5a68818ce4c3bd634
Nick Foden <nickfoden@gmail.com>
2017-08-29 16:54:26 -0400

    Added dropzone and started to add Cloudinary

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react'
+import Dropzone from 'react-dropzone'
 
 class Images extends Component {
-    
+
+        uploadFile(files){
+            console.log('uploadFile: ')
+            const image = files[0]
+
+            const cloudName = 'proofer'
+            const url = 'https://api.cloudinary.com/v1_1'+cloudName+'/image/upload'
+
+
+        }
     render(){
         return (
             <div>
                 Images Component
+                <Dropzone onDrop={this.uploadFile.bind(this)} />
             </div>
         )
     }
 }
+
+/*https://youtu.be/WOTFmPkWbxo?t=52m */
 
 export default Images
